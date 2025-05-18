0d575540b51e8930fdd2f703bbca821926f6f208
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2025-05-17 23:00:11 -0400

    projects fix

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -2,7 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jkwifqnzrfsybbvvqmms.supabase.co';
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imprd2lmcW56cmZzeWJidnZxbW1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1OTY1OTAsImV4cCI6MjA2MjE3MjU5MH0.C1Tjy1nNJbmJ0gmor4H5v0JIANyq9vxgt_9y34NNFM4;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
